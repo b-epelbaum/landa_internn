bb9b70dacea31e83a2d58b8d3514aa941f933312
Claupacius <greenparrot5@gmail.com>
2018-09-02 20:10:05 +0300

    Fix

--- src/libs/frameProviders/offlineCalcProvLib/offreader.cpp
+++ src/libs/frameProviders/offlineCalcProvLib/offreader.cpp
@@ -133,7 +133,7 @@ FRAME_PROVIDER_ERROR OfflineReader::init()
 	_lastAcquiredImage = -1;
 	_imagePaths.clear();
 
-	const auto imageFolder = "c:\\Temp\\Wave";//_SourceFolderPath;
+	const auto imageFolder = _SourceFolderPath;
 	if ( !QFileInfo(imageFolder).exists())
 	{
 		return FRAME_PROVIDER_ERROR::ERR_OFFLINEREADER_SOURCE_FOLDER_INVALID;
